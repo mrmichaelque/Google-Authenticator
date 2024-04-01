6c52b262e0091c7032fd35a3e8d12f92339ac3c4
M C Q <129240033+mrmichaelque@users.noreply.github.com>
2024-04-01 17:19:21 -0400

    Commit src files

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className='App'>
+      <header className='App-header'>
+        <p>
+          Case Management Web Server Application
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
